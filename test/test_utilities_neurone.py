e55acc688361ccf38774696321542eae396228ff
Felix Heilmeyer <code@fehe.eu>
2018-11-16 13:06:34 +0100

    test data loading

--- test/test_utilities_neurone.py
+++ test/test_utilities_neurone.py
@@ -11,22 +11,13 @@
 from unittest import TestCase
 import pickle
 import os
+from hashlib import sha256
+from multiprocessing import Pool
 
 from neurone_loader.neurone import *
 
 data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_data')
 sessions = ['ses1', 'ses2']
-
-
-class TestReadMetadata(TestCase):
-    def test_read_neurone_protocol(self):
-        for session in sessions:
-            with open(os.path.join(data_path, '{}_meta.pkl'.format(session)),
-                      'rb') as f:
-                true_session_meta = pickle.load(f)
-
-            session_meta = read_neurone_protocol(os.path.join(data_path, session))
-            self.assertEqual(true_session_meta, session_meta)
 
 
 class TestDataReading(TestCase):
@@ -47,3 +38,65 @@ class TestDataReading(TestCase):
                 expected_samples = sampling_rate * duration.total_seconds()
                 self.assertAlmostEqual(n_samples_from_info, expected_samples,
                                        delta=sampling_rate * 0.1)
+
+    def test_empty_file(self):
+        session_path = os.path.join(data_path, 'empty_session')
+        events = read_neurone_events(session_path, sampling_rate=1)
+        self.assertEqual(events, {'events': [], 'dtype': []})
+
+
+class TestStability(TestCase):
+    def test_event_stability(self):
+        with open(os.path.join(data_path, 'ses1_1_events.pkl'), 'rb') as f:
+            true_events = pickle.load(f)
+        read_events = read_neurone_events(os.path.join(data_path, sessions[0]))
+        # noinspection PyUnresolvedReferences
+        self.assertTrue((true_events['events'] == read_events['events']).all())
+        self.assertEqual(true_events['dtype'], read_events['dtype'])
+        self.assertEqual(true_events.keys(), read_events.keys())
+
+    def test_protocol_stability(self):
+        for session in sessions:
+            with open(os.path.join(data_path, '{}_meta.pkl'.format(session)),
+                      'rb') as f:
+                true_session_meta = pickle.load(f)
+
+            session_meta = read_neurone_protocol(os.path.join(data_path, session))
+            self.assertEqual(true_session_meta, session_meta)
+
+    def test_data_stability(self):
+        hashes = {'ses1:1': '4c04a0071d7f0dfd6bd337bb5cabf7a33f6d3a5a57d93de1926c8d42fda2725b',
+                  'ses1:2': '76b8fdf3f951c7b357a4a59c719669058925a432e7e7a6a749dcc64b13925086',
+                  'ses1:3': 'c1782cecded74fb7567a9c9c159d33fbcbc67e9f0def4e3b27b0d2814d352ec9',
+                  'ses1:4': 'fc29a8911987c6a7667539ebe547ff76bfd0eae7f29a49cdb8279d1d7f402876',
+                  'ses2:1': '887012a3f8449a3dbe87cab0e7d8411ee1989cdffadf9b46ee05d7619bbaf593',
+                  'ses2:2': '2dbede3c93f4b37112ebee9798d02937e85e3f31a3bcf3d987222fed67d09379',
+                  'ses2:3': 'e019b3124a1b2f159283d897aca7b1821676a3c2d5e12dfb7ab0a2b8cac3f892',
+                  'ses2:4': '4af04066ed8651c4cec352f23467258f4acb8091385af24bb43db18e9179dbaf'}
+
+        pool = Pool()
+        results = []
+
+        for session in sessions:
+            session_path = os.path.join(data_path, session)
+            session_meta = read_neurone_protocol(session_path)
+            phases = session_meta['phases'][:-1] if session == session[-1] else session_meta['phases']
+            for phase in phases:
+                number = phase['number']
+                results.append(pool.apply_async(_hash_data,
+                                                (session, session_path, number)))
+
+            if session == sessions[-1]:
+                key, shasum = _hash_data(session, session_path,
+                                         session_meta['phases'][-1]['number'])
+                self.assertEqual(hashes[key], shasum)
+
+        for result in results:
+            key, shasum = result.get(timeout=20)
+            self.assertEqual(hashes[key], shasum)
+
+
+def _hash_data(session, session_path, number):
+    key = ':'.join([session, number])
+    session_data = read_neurone_data(session_path, number)
+    return key, sha256(session_data.data.tobytes()).hexdigest()
