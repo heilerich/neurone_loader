96ea6b8987d08265b92a99e07c06970f0d2cb8f5
Felix Heilmeyer <code@fehe.eu>
2018-11-16 16:22:36 +0100

    documentation and cleanup for export module

--- neurone_loader/mne_export.py
+++ neurone_loader/mne_export.py
@@ -7,31 +7,80 @@
 #  https://opensource.org/licenses/mit-license.php                             -
 #  Please see the file LICENSE for details.                                    -
 # ------------------------------------------------------------------------------
-
+"""
+This module provides the metaclass `MneExportable` that allows subclasses implementing all the metaclass's properties
+to be converted to a mne.io.RawArray.
+"""
 
 import logging
 import numpy as np
+import abc
 
 logger = logging.getLogger(__name__)
 
 
-class MneExportable:
+class MneExportable(abc.ABC):
+    """
+
+    A metaclass that provides a function allowing objects that expose data, events, channels and sampling_rate
+    properties to be converted to an mne.io.RawArray.
+
+    """
+
+    @property
+    @abc.abstractmethod
+    def data(self):
+        """
+        :return: should contain data in (n_samples, n_channels) shape
+        :rtype: numpy.ndarray
+        """
+        pass
+
+    @property
+    @abc.abstractmethod
+    def events(self):
+        """
+        :return: should contain the events as a DataFrame, required fields are `StartSampleIndex`, `StopSampleIndex`
+        and `Code`. Additional fields are ignored.
+        :rtype: pandas.DataFrame
+        """
+        pass
+
+    @property
+    @abc.abstractmethod
+    def channels(self):
+        """
+        :return: should contain the names of channels, matching the sequence used in the data property
+        :rtype: list[str]
+        """
+        pass
+
+    @property
+    @abc.abstractmethod
+    def sampling_rate(self):
+        """
+        :return: should contain the used sampling rate
+        :rtype: int
+        """
+        pass
+
     def _import_mne(self):
         try:
+            # noinspection PyPackageRequirements
             import mne
             self._mne = mne
             return True
         except ImportError:
-            logger.error("To convert data to an MNE object you must install MNE" \
+            logger.error("To convert data to an MNE object you must install MNE"
                          "and all its dependencies.")
             return False
 
     def to_mne(self):
         """
-        Convert loaded NeurOne data to a mne.io.RawArray
-
-        Returns:
-           - the converted mne.io.RawArray
+        Convert loaded data to a mne.io.RawArray
+        :return: the converted data
+        :rtype: mne.io.RawArray
+        :raises ImportError: if the mne package is not installed
         """
 
         if not hasattr(self, '_mne'):
@@ -43,14 +92,15 @@ class MneExportable:
         data = (self.data / (1000 * 1000)).T
         events = self.events
 
-        info = mne.create_info(ch_names = self.channels,
-                               sfreq = self.sampling_rate,
+        info = mne.create_info(ch_names=self.channels,
+                               sfreq=self.sampling_rate,
                                ch_types='eeg')
 
         cnt = mne.io.RawArray(data, info)
 
         stim_channel = np.zeros_like(cnt.get_data()[0])
-        ssc = [(start, stop, code) for (start, stop, code) in events[['StartSampleIndex', 'StopSampleIndex', 'Code']].values]
+        ssc = [(start, stop, code) for (start, stop, code)
+               in events[['StartSampleIndex', 'StopSampleIndex', 'Code']].values]
 
         for start, stop, code in ssc:
             stim_channel[start:stop+1] += code
