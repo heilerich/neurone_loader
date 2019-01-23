b4d3a111475c514cc28bd7d583afdd1c79c3cbd9
heilerich <code@fehe.eu>
2019-01-23 15:05:29 +0100

    add changelog, single source version number

--- neurone_loader/__init__.py
+++ neurone_loader/__init__.py
@@ -14,3 +14,5 @@ Use :py:class:`Recording`, :py:class:`Session` or :py:class:`Phase` to represent
 """
 
 from .loader import Recording, Session, Phase
+
+__version__ = '1.0.0'
--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
 #  This file (setup.py) is part of neurone_loader                              -
 #  (https://www.github.com/heilerich/neurone_loader)                           -
-#  Copyright © 2018 Felix Heilmeyer.                                           -
+#  Copyright © 2019 Felix Heilmeyer.                                           -
 #                                                                              -
 #  This code is released under the MIT License                                 -
 #  https://opensource.org/licenses/mit-license.php                             -
@@ -10,14 +11,45 @@
 
 from setuptools import setup
 
+import os
+import codecs
+import re
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+def read(*parts):
+    # noinspection PyArgumentEqualDefault
+    with codecs.open(os.path.join(this_directory, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+version_number = find_version('neurone_loader', '__init__.py')
+
 setup(name='neurone_loader',
-      version='0.1',
+      version=version_number,
       description='Utilities for loading data recorded with NeurOne',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
       url='http://github.com/heilerich/neurone_loader',
       author='Felix Heilmeyer',
       author_email='code@fehe.eu',
       license='MIT',
       packages=['neurone_loader'],
+      keywords=['EEG', 'science', 'neuroscience', 'neurone', 'bittium', 'megaemg', 'MNE'],
+      download_url='https://github.com/heilerich/neurone_loader/archive/v{}.tar.gz'.format(version_number),
       zip_safe=False,
       install_requires=[
           'numpy',
