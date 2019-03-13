541becba1ac15dee0f2c5f7dffa265fec7824e04
Felix Heilmeyer <code@fehe.eu>
2019-03-13 12:01:47 +0100

    unified logging and added skin resistance channel to category bio

---
+++ neurone_loader/util.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#  This file (util.py) is part of neurone_loader                               -
+#  (https://www.github.com/heilerich/neurone_loader)                           -
+#  Copyright © 2019 Felix Heilmeyer.                                           -
+#                                                                              -
+#  This code is released under the MIT License                                 -
+#  https://opensource.org/licenses/mit-license.php                             -
+#  Please see the file LICENSE for details.                                    -
+# ------------------------------------------------------------------------------
+
+import logging
+import sys
+
+
+def get_logger():
+    """
+    Get the default logger for this module and set default settings.
+
+    :return: the logger
+    :rtype: logging.Logger
+    """
+    lgr = logging.getLogger('neurone_loader')
+    lgr.setLevel(logging.DEBUG)
+    fmt = logging.Formatter(fmt='%(asctime)s [%(name)s.%(module)s]:%(levelname)s: %(message)s', datefmt='%I:%M:%S')
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.NOTSET)
+    stdout_handler.setFormatter(fmt)
+    lgr.addHandler(stdout_handler)
+    return lgr
+
+
+logger = get_logger()
